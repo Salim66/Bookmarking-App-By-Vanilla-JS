f614ba7b7f2333d52159042251026ac08db9234b
Salim66 <salimhasanriad8@gmail.com>
2021-08-28 11:22:26 +0600

    floating javascript basic functionality add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
 // grab everything we need
-
+const body      = document.body;
+const input     = document.querySelector('input[type=text]');
+const overlay   = document.querySelector('.overlay');
 
 
 // define all functions w'll need
+function showFloater(){
+    body.classList.add('show-floater');
+}
 
+function closeFloater(){
+    if(body.classList.contains('show-floater')){
+        body.classList.remove('show-floater');
+    }
+}
 
 
 // add event linsteners
+input.addEventListener('focus', showFloater);
+overlay.addEventListener('click', closeFloater);
