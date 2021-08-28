ffe8196e853b32530d1339f005fb61d26c8b5233
Salim66 <salimhasanriad8@gmail.com>
2021-08-28 12:23:16 +0600

    Showing bookmark

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,11 @@ const body      = document.body;
 const input     = document.querySelector('input[type=text]');
 const overlay   = document.querySelector('.overlay');
 
+// bookmark
+const bookmark_list     = document.querySelector('.bookmark-list');
+const bookmark_form     = document.querySelector('.bookmark-form');
+const bookmark_input    = document.querySelector('input[type=text]');
+
 
 // define all functions w'll need
 function showFloater(){
@@ -15,7 +20,25 @@ function closeFloater(){
     }
 }
 
+// bookmark
+function createBookmark(e){
+    e.preventDefault();
+    
+    let title           = bookmark_input.value;
+    let bookmark        = document.createElement('a'); 
+    bookmark.className  = 'bookmark';
+    bookmark.innerHTML  = title;
+    bookmark.href       = '#';
+    bookmark.target     = '_blank';
+    bookmark_list.appendChild(bookmark);
+
+    bookmark_form.reset();
+}
+
 
 // add event linsteners
-input.addEventListener('focus', showFloater);
+input.addEventListener('focusin', showFloater);
+input.addEventListener('focusout', closeFloater);
 overlay.addEventListener('click', closeFloater);
+// bookmark
+bookmark_form.addEventListener('submit', createBookmark);
